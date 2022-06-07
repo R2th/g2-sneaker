2489023e2c73dfd7348171326814c3f2275ddcb7
R2th <haupro186@gmail.com>
2022-06-08 02:49:13 +0700

    fix empty card

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,17 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import CartItems from "../CartItems";
 
 const Cart = ({ data, setData, setIsSthRemove }) => {
   return (
     <div className="cart-items">
-      {data &&
+      {data ? (
         data.map((item) => (
           <CartItems
             data={item}
             setData={setData}
             setIsSthRemove={setIsSthRemove}
           />
-        ))}
+        ))
+      ) : (
+        <>Your cart is empty.</>
+      )}
     </div>
   );
 };
